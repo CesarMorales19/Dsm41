cf535901460415935b979e45d2428972a249278f
CesarMorales19 <al222311022@gmail.com>
2024-09-25 21:46:12 -0600

    Falla

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // Asegúrate de que esto esté correcto
+use Faker\Factory as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // Crear una instancia de Faker
+        $faker = Faker::create();
+
+        // Generar 50 posts ficticios
+        foreach (range(1, 50) as $index) {
+            DB::table('posts')->insert([ // Cambia [ a ->insert([
+                'title' => $faker->sentence,
+                'content' => $faker->paragraph,
+            ]);
+        }
+    }
+}
